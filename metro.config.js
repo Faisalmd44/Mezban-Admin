425d473c5b9afef4f74681815b8defa14a8d117c
Mohammad faisal <faisalmd44@gmail.com>
2026-07-23 22:11:47 +0530

    Fix Metro config

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,7 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
-const { FileStore } = require("metro-cache");
 
 const config = getDefaultConfig(__dirname);
 
-const root =
-  process.env.METRO_CACHE_ROOT || path.join(__dirname, ".metro-cache");
-
-config.cacheStores = [new FileStore({ root: path.join(root, "cache") })];
-config.maxWorkers = 2;
+config.resolver.unstable_enablePackageExports = true;
 
 module.exports = config;
